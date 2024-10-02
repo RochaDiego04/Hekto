9d996a18a12cb58d265574348fb278595919cb06
Diego Rocha <drocha@griddynamics.com>
2024-10-02 14:51:19 -0600

    feat [navigation]: Adding routing for application and not found page view

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,14 @@
+import { NavLink } from "react-router-dom";
+
+export default function NavBar() {
+  return (
+    <nav>
+      <ul>
+        <li>
+          {/* <NavLink to="/">Home</NavLink>
+          <NavLink to="/asdfa">NotFound</NavLink> */}
+        </li>
+      </ul>
+    </nav>
+  );
+}
---
+++ src/pages/NotFound/NotFound.tsx
@@ -0,0 +1,28 @@
+import { NavLink } from "react-router-dom";
+import HektoLogo from "../../assets/HektoLogo";
+import Button from "../../components/Button/Button";
+
+export default function NotFound() {
+  return (
+    <div className=" text-center mt-40">
+      <NavLink to="/">
+        <HektoLogo
+          width="100px"
+          height="100px"
+          className=" flex justify-center items-center mx-auto"
+        ></HektoLogo>
+      </NavLink>
+
+      <h1 className=" inline-block max-w-4xl">
+        <span className=" text-primary">Oops!</span> We couldn't found the page
+        you're trying to reach
+      </h1>
+      <p className=" text-xl">
+        The link you followed may be broken, or the page may have been removed.
+      </p>
+      <Button mode="filled" className=" mt-8">
+        <NavLink to="/">Home</NavLink>
+      </Button>
+    </div>
+  );
+}
