a6de7a3837bbde7afaf0960f1ff7c95e7fd07871
Diego Rocha <drocha@griddynamics.com>
2024-10-18 15:01:30 -0600

    feat [products-view]: Products catalog layout toggle working along the displayed cards

--- src/pages/Products/ProductsPage.tsx
+++ src/pages/Products/ProductsPage.tsx
@@ -13,14 +13,24 @@ export default function ProductsPage() {
   }, []);
 
   return (
-    <section className=" p-maxContainer md:p-24">
-      <ProductCatalogOptions
-        productsLayout={productsLayout}
-        onSetProductsLayout={handleSetProductsLayout}
-      ></ProductCatalogOptions>
-      <CheckboxFilters></CheckboxFilters>
-      <MainProductCatalog></MainProductCatalog>
-      {/* product catalog component with the productsLayout prop */}
+    <section className="p-maxContainer md:p-24 grid grid-cols-1 md:grid-cols-[auto_1fr] gap-x-16">
+      {/* Full width row */}
+      <div className="col-span-1 md:col-span-2">
+        <ProductCatalogOptions
+          productsLayout={productsLayout}
+          onSetProductsLayout={handleSetProductsLayout}
+        />
+      </div>
+
+      {/* Filters column */}
+      <div className="col-span-1">
+        <CheckboxFilters />
+      </div>
+
+      {/* Products catalog column */}
+      <div className="col-span-1">
+        <MainProductCatalog productsLayout={productsLayout} />
+      </div>
     </section>
   );
 }
