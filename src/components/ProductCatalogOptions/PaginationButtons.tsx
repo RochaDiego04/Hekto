eabba66ceedc7483e2b152d75c47cc3bd9e5de07
Diego Rocha <drocha@griddynamics.com>
2024-10-22 11:11:31 -0600

    fix [products-catalog]: Pagination buttons already working

--- src/components/ProductCatalogOptions/PaginationButtons.tsx
+++ src/components/ProductCatalogOptions/PaginationButtons.tsx
@@ -1,5 +1,7 @@
-import { useAppSelector } from "../../store/hooks";
 import { useDispatch } from "react-redux";
+import { useAppSelector } from "../../store/hooks";
+import { setCurrentPage } from "../../store/filter-slice";
+import Button from "../Button/Button";
 
 type PaginationButtonsProps = {
   totalItems: number | undefined;
@@ -9,22 +11,29 @@ export default function PaginationButtons({
   totalItems,
 }: PaginationButtonsProps) {
   const dispatch = useDispatch();
+  const { currentPage, itemsPerPage } = useAppSelector((state) => state.filter);
 
   if (!totalItems) return null;
 
+  const totalPages = Math.ceil(totalItems / itemsPerPage);
+
+  const handlePageChange = (page: number) => {
+    dispatch(setCurrentPage(page));
+  };
+
   return (
-    <div className="flex items-center justify-center gap-2 mt-4">
-      {/* {Array.from({ length: totalPages }, (_, i) => (
-        <button
+    <div className="flex items-center justify-end gap-2 mt-4">
+      {Array.from({ length: totalPages }, (_, i) => (
+        <Button
           key={i + 1}
           onClick={() => handlePageChange(i + 1)}
           className={`px-4 py-2 border ${
-            currentPage === i + 1 ? "bg-gray-300" : ""
+            currentPage === i + 1 ? "bg-primary text-white" : ""
           }`}
         >
           {i + 1}
-        </button>
-      ))} */}
+        </Button>
+      ))}
     </div>
   );
 }
