509bdfda15b69602f13df979032437a850d15ccd
Diego Rocha <drocha@griddynamics.com>
2024-10-23 15:24:46 -0600

    fix [test]: Dropdown gets closed when clicking outside, test implemented

--- src/components/Dropdown/SelectDropdown.test.tsx
+++ src/components/Dropdown/SelectDropdown.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import SelectDropdown from "./SelectDropdown";
 
@@ -16,5 +16,48 @@ describe("SelectDropdown Component", () => {
     // Check if the placeholder is visible
     const placeholderElement = screen.getByText("Select an option");
     expect(placeholderElement).toBeInTheDocument();
+  });
+  test("opens the dropdown when clicked", () => {
+    render(
+      <SelectDropdown
+        value=""
+        onChange={() => {}}
+        placeholder="Select an option"
+        options={["Option 1", "Option 2"]}
+      />
+    );
+
+    // options closed initially
+    expect(screen.queryByText("Option 1")).not.toBeInTheDocument();
+
+    // Click on the dropdown
+    const dropdown = screen.getByText("Select an option");
+    fireEvent.click(dropdown);
+
+    // options opened
+    expect(screen.getByText("Option 1")).toBeInTheDocument();
+  });
+  test("closes the dropdown when clicking outside", () => {
+    render(
+      <div>
+        <SelectDropdown
+          value=""
+          onChange={() => {}}
+          placeholder="Select an option"
+          options={["Option 1", "Option 2"]}
+        />
+        <div data-testid="outside">Outside Element</div>
+      </div>
+    );
+
+    // Open the dropdown
+    fireEvent.click(screen.getByText("Select an option"));
+    expect(screen.getByText("Option 1")).toBeInTheDocument();
+
+    // Click outside the dropdown
+    fireEvent.mouseDown(screen.getByTestId("outside"));
+
+    // Expect the dropdown to close
+    expect(screen.queryByText("Option 1")).not.toBeInTheDocument();
   });
 });
